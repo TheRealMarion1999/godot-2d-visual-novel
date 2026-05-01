dacb57100dbb7dcc191245473a8fe0bcbb87011b
Marion <sjniesen@madisoncollege.edu>
2026-05-01 08:38:43 -0500

    uh

--- godot/Backgrounds/Background.gd
+++ godot/Backgrounds/Background.gd
@@ -5,3 +5,4 @@ extends Resource
 @export var id := "background_id"
 @export var display_name := "Display Name"
 @export var texture: Texture2D
+#Literaly just putting this here to try and salvage my work oh god oh fuck
